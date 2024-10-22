f414f8c27d894ebf9c04178814458e3b862425df
F1zzTao <timurbogdanov2008@gmail.com>
2024-10-22 13:56:36 +0300

    Moved files, replaced raw token input with `.env` file

--- config.py
+++
@@ -1,3 +0,0 @@
-TOKEN = 'VK GROUP API TOKEN'
-CLUBPREF = '[club224599461|@ampersand_bot]'
-DB_PATH = './database.db'
---
+++ src/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("VK_GROUP_TOKEN")
+
+CLUBPREF = '[club224599461|@ampersand_bot]'
+DB_PATH = './database.db'
